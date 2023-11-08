1c8ba83fdceedf18f734c7ad06f797c55be20cb1
Lungelo Mbalane <Mbalanel@getsavvi.co.za>
2023-11-08 14:35:45 +0200

    Add project files.

---
+++ StackOverflowProject/App_Start/UnityConfig.cs
@@ -0,0 +1,26 @@
+using StackOverflowProject.ServiceLayer;
+using System.Web.Http;
+using Unity;
+using Unity.WebApi;
+
+namespace StackOverflowProject
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            container.RegisterType<IQuestionsService, QuestionsService>();
+            container.RegisterType<IAnswersService, AnswersService>();
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+        }
+
+       
+    }
+}
